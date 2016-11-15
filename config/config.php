229156e7640ddd15eee90d21ab988297ef95fe11
MatthieuSchmit <Matthieu@Aquila.sitecom.router>
2016-11-16 00:36:36 +0100

    test

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The Big Hub
+ * @authors Matth Schmit (@MatthSchmit), Tim Chapelle (@timchapelle)
+ * https://github.com/thebighub
+ *
+ * Fichier de configuration de l'application
+ */
+return [
+    "dbUser" => "root",
+    "dbPass" => "isfce@2015",
+    "dbHost" => "localhost",
+    "dbName" => "bighub"
+];
